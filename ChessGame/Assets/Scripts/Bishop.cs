6b7bb8beb6187d97213c79208f3fefd9b7eab629
steveod0701 <steveod0929@gmail.com>
2017-10-28 00:41:56 +0900

    Add project and android apk
    added project and android apk file

---
+++ ChessGame/Assets/Scripts/Bishop.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bishop : Chessman
+{
+	public override bool[,] PossibleMove()
+    {
+        bool[,] r = new bool[8, 8];
+
+        Chessman c;
+        int i, j;
+
+        //TopLeft
+        i = CurrentX;
+        j = CurrentY;
+
+        while (true)
+        {
+            i--;
+            j++;
+            if (i < 0 || j >= 8)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, j];
+            if (c == null)
+                r[i, j] = true;
+            else
+            {
+                if (isWhite != c.isWhite)
+                    r[i, j] = true;
+                break;
+            }
+        }
+
+        //TopRight
+        i = CurrentX;
+        j = CurrentY;
+
+        while (true)
+        {
+            i++;
+            j++;
+            if (i >= 8 || j >= 8)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, j];
+            if (c == null)
+                r[i, j] = true;
+            else
+            {
+                if (isWhite != c.isWhite)
+                    r[i, j] = true;
+                break;
+            }
+        }
+
+        //Down Left
+        i = CurrentX;
+        j = CurrentY;
+
+        while (true)
+        {
+            i--;
+            j--;
+            if (i <0 || j < 0)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, j];
+            if (c == null)
+                r[i, j] = true;
+            else
+            {
+                if (isWhite != c.isWhite)
+                    r[i, j] = true;
+                break;
+            }
+        }
+
+        //Down Right
+        i = CurrentX;
+        j = CurrentY;
+
+        while (true)
+        {
+            i++;
+            j--;
+            if (i >= 8 || j < 0)
+                break;
+
+            c = BoardManager.Instance.Chessmans[i, j];
+            if (c == null)
+                r[i, j] = true;
+            else
+            {
+                if (isWhite != c.isWhite)
+                    r[i, j] = true;
+                break;
+            }
+        }
+
+        return r;
+    }
+}
